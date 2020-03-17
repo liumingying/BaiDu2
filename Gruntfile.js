68c90128432da86b05e2a088495f04ea60d6356d
yingzi <yingzi@example.com>
2020-03-17 17:18:19 +0800

    copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,34 @@
 /* global module: true */
 module.exports = function (grunt) {
-    grunt.initConfig({
-      htmlmin: {
-        options: {
-          collapseWhitespace: true,
-          preserveLineBreaks: false
-        },
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseWhitespace: true
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/baidu.css': 'baidu.css'
+    },
+    uglify: {
+      release:{
         files: {
-          src: './index.html',
-          dest: 'dist/index.html'
-        }
-      },
-      cssmin: {
-        'dist/baidu.css': 'baidu.css'
-      },
-      uglify: {
-        release:{
-          files: {
-            'dist/baidu.js': 'baidu.js'
-          }
+          'dist/baidu.js': 'baidu.js'
         }
       }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
-  };
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask("release", ['copy', 'htmlmin','cssmin', 'uglify']);
+};
